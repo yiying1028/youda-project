2f143e927bcac736916f16d5f8e0f5888fdff7c2
yiying1028 <yiyingsong1028@gmail.com>
2026-04-12 16:43:54 +0800

    feat: 添加 MybatisMetaObjectHandler 以支持自动填充时间戳，更新 Dockerfile 以使用自定义 Maven 设置

--- youda-backend/src/main/java/com/youda/service/impl/CourseServiceImpl.java
+++ youda-backend/src/main/java/com/youda/service/impl/CourseServiceImpl.java
@@ -1,4 +1,4 @@
-﻿package com.youda.service.impl;
+package com.youda.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
